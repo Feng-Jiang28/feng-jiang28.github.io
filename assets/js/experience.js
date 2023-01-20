c450b7db2a0ad52170d89e8c97e2e1503caaee43
jiang_feng <fjiang44@usc.edu>
2023-01-19 19:47:59 -0800

    first commit

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -6,9 +6,9 @@ const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
     title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
+    cardImage: "assets/images/experience-page/MetaApp.jpeg",
+    place: "Meta App",
+    time: "(May, 2022 - Aug, 2022)",
     desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
   },
   {
